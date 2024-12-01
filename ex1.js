a5e983d04d62a8d21f0b010957690379ee7c0ae3
foadsh1 <fo2ad.the.king@gmail.com>
2024-12-01 22:44:04 +0200

    Add files via upload

---
+++ ex1.js
@@ -0,0 +1,7 @@
+//Students names:Foad shalata  ID:314743097       Rabea bader   ID:
+
+const num = 237;
+
+let result = Number(num % 2 == 0) + Number(num % 3 == 0) + Number(num % 5 == 0);
+
+console.log(result);
